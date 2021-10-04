c77c3cc64d8d4542a5a6cf3e1c2eaaa6b5035a6a
Andrej Sabol <32419756+mrAsabol@users.noreply.github.com>
2021-10-04 18:57:07 +0200

    Update baza.js
    removed old code

--- foodshop/js/baza.js
+++ foodshop/js/baza.js
@@ -17,11 +17,4 @@ $(document).ready(function() {
        console.log(meal[0].meal_name); 
     });
     
-    //Hactoberfest funkcija
-    function zaObrisati() {
-        for(var i=0;i<3;i++){
-        console.log("obriši me nakon hacktoberfesta");
-        }
-    };
-    
 });
